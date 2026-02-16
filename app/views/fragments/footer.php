f46d37271552a066b14d67147352ea0633acc268
Andrianinandroid <raharisonnyavo02@gmail.com>
2026-02-16 15:39:20 +0300

    refactorisation views

---
+++ app/views/fragments/footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="footer clearfix mb-0 text-muted">
+        <div class="container-fluid">
+            <div class="row align-items-center text-center text-md-start">
+
+                <!-- Colonne 1 -->
+                <div class="col-md-4">
+                    <p class="mb-0">
+                        <i class="bi bi-person-fill text-primary"></i>
+                        <strong>ETU4086</strong>
+                    </p>
+                </div>
+
+                <!-- Colonne 2 -->
+                <div class="col-md-4">
+                    <p class="mb-0">
+                        <i class="bi bi-person-fill text-success"></i>
+                        <strong>ETU4045</strong>
+                    </p>
+                </div>
+
+                <!-- Colonne 3 -->
+                <div class="col-md-4 text-md-end">
+                    <p class="mb-0">
+                        <i class="bi bi-person-fill text-warning"></i>
+                        <strong>ETU3950</strong>
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ app/views/fragments/sidebar.php
@@ -0,0 +1,27 @@
+<div id="sidebar">
+        <div class="sidebar-wrapper active">
+            <div class="sidebar-header">
+                <h5 class="mt-2">Gestion Dons</h5>
+            </div>
+
+            <div class="sidebar-menu">
+                <ul class="menu">
+                    <li class="sidebar-title">Menu</li>
+
+                    <li class="sidebar-item">
+                        <a href="/dash" class="sidebar-link">
+                            <i class="bi bi-grid-fill"></i>
+                            <span>Tableau de Bord</span>
+                        </a>
+                    </li>
+
+                    <li class="sidebar-item">
+                        <a href="/donate" class="sidebar-link">
+                            <i class="bi bi-box-seam-fill"></i>
+                            <span>Collecte de dons</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
