8c23db456e3acaa71333ae03aaa4eb6d803cd92a
Nomena-coder <raobelinanomenjanahary@gmail.com>
2026-02-16 17:11:25 +0300

    Fonction dispatch + model + controller

--- app/controllers/DonController.php
+++ app/controllers/DonController.php
@@ -5,13 +5,36 @@
     use Flight;
     use app\DonModel;
     use app\BesoinVilleModel;
+    use app\DispatchModel;
 
     class DonController {
 
         public function dispatch() {
-            $don = new DonModel(Flight::db());
-            $bvm = new BesoinVilleModel(Flight::db());
+            $db = Flight::db();
+            $don = new DonModel($db);
+            $bvm = new BesoinVilleModel($db);
+            $dm = new DispatchModel($db);
             $ordered = $bvm->getByDateDesc();
+            $dispo = $don->getDonsDispo();
+            $count = 0;
+            try {
+                $db->beginTransaction();
+                foreach($dispo as $d) :
+                    if($count == count($ordered)) :
+                        $count == 0;
+                    endif;
+                    $data = [
+                        $ordered[$count]['id_ville'],
+                        $d['id']
+                    ];
+                    $dm->insert($data);
+                    $count++;
+                endforeach;
+                $db->commit();
+            } catch(Exception $e) {
+                $db->rollback();
+                $error = "Error while dispatching...";
+            }
         }    
 
     }
