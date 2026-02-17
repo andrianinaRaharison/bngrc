bfc1ca4d4b499930e913aeee124d9a2dd423882c
Indra-Ramp <ramparany.indra@gmail.com>
2026-02-17 07:47:49 +0300

    recapitulation

--- database/20260217-2-donargent.sql
+++ database/20260217-2-donargent.sql
@@ -3,5 +3,5 @@ INSERT INTO unite (ref) VALUES ('ar');
 INSERT INTO objets (libelle, id_unite) VALUES 
 ('Argent', 5);
 
-INSERT INTO dons (id_objet, daty, quantite, type_besoin) VALUES 
-(6, '2026-02-16 08:00:00', 5000000, 3);
+INSERT INTO dons (id_objet, daty, quantite, id_type) VALUES 
+(6, '2026-02-16 08:00:00', 5000, 3);
